a8ee2e7d11e955939fa7f29dff0fbd09be5fd2d4
Ngo Khoi <ngokhoivn@gmail.com>
2025-07-05 10:30:24 +0700

    Bai_77

--- Source/Crunch/Crunch.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Crunch.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Crunch, "Crunch" );
